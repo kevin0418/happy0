37cd88fd4ee031b56164e361b513557abf0607c0
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2025-05-27 21:47:32 +1000

    add file
    add file

---
+++ happy.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+st.write("# 행복에  투자 해라! 👋")
+
+# with st.sidebar:
+     
+#      st.markdown(
+#       """
+#           언어 번역기 
+      
+#           - [Google 번역 ](https://translate.google.com/?sl=ko&tl=en&op=translate)
+#           - [파파고 번역 ](https://papago.naver.com/)
+          
+                          
+#     """
+#     )
+     
+
+st.markdown(
+    """
+    행복은 어디애 있나요,  만들어야 하나요   
+    여기 Kevin 이 맛깔나게   여러 각도에서 풀어 봅니다.
+
+
+    - [Video 를 통해 보기 ](https://youtu.be/Qi89LZ4nwBg)
+    
+    - [대화 를 통해 듣기](https://notebooklm.google.com/notebook/ac3633d5-e076-49eb-866b-2d3a3eb69658/audio)
+    
+    
+   
+    
+    
+
+     
+"""
+)
